6d305492e62d900090218d00989d1e66918061c8
San <aeginventor@gmail.com>
2025-12-30 00:40:11 +0900

    feat: useReducer를 이용한 새로운 연락처 등록 기능 구현

--- mission/day15/src/App.jsx
+++ mission/day15/src/App.jsx
@@ -1,16 +1,52 @@
 import "./App.css";
+import { useRef, useReducer } from "react";
 import ContactEditor from "./components/ContactEditor";
 import ContactList from "./components/ContactList";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    default:
+      return state;
+  }
+}
+
+const mockData = [
+  {
+    id: 0,
+    name: "아무개",
+    email: "example@gmail.com",
+  },
+];
+
 function App() {
+  const [contacts, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(1);
+
+  const onCreate = (contents) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        name: contents.name,
+        email: contents.email,
+      },
+    });
+  };
+
+  const onDelete = () => {
+    dispatch();
+  };
+
   return (
     <div className="App">
       <h2>Contact List</h2>
       <section>
-        <ContactEditor />
+        <ContactEditor onCreate={onCreate} />
       </section>
       <section>
-        <ContactList />
+        <ContactList contacts={contacts} onDelete={onDelete} />
       </section>
     </div>
   );
--- mission/day15/src/components/ContactEditor.jsx
+++ mission/day15/src/components/ContactEditor.jsx
@@ -1,14 +1,75 @@
+import { useReducer, useRef } from "react";
 import "./ContactEditor.css";
 
-export default function ContactEditor() {
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_NAME":
+      return { ...state, name: action.value };
+    case "SET_EMAIL":
+      return { ...state, email: action.value };
+    case "RESET":
+      return { name: "", email: "" };
+    default:
+      return state;
+  }
+}
+
+export default function ContactEditor({ onCreate }) {
+  const [contents, dispatch] = useReducer(reducer, { name: "", email: "" });
+  const nameRef = useRef();
+  const emailRef = useRef();
+
+  const handleNameChange = (e) => {
+    dispatch({
+      type: "SET_NAME",
+      value: e.target.value,
+    });
+  };
+
+  const handleEmailChange = (e) => {
+    dispatch({
+      type: "SET_EMAIL",
+      value: e.target.value,
+    });
+  };
+
+  const handleAddContact = () => {
+    if (contents.name === "") {
+      nameRef.current.focus();
+      return;
+    } else if (contents.email === "") {
+      emailRef.current.focus();
+      return;
+    }
+
+    onCreate(contents);
+
+    dispatch({
+      type: "RESET",
+      value: "",
+    });
+  };
+
   return (
     <div className="ContactEditor">
       <div className="title">Add Contact</div>
       <div className="input_wrapper">
-        <input className="name" placeholder="이름 ..." />
-        <input className="contact" placeholder="연락처(이메일) ..." />
+        <input
+          className="name"
+          placeholder="이름 ..."
+          onChange={handleNameChange}
+          ref={nameRef}
+          value={contents.name}
+        />
+        <input
+          className="contact"
+          placeholder="연락처(이메일) ..."
+          onChange={handleEmailChange}
+          ref={emailRef}
+          value={contents.email}
+        />
       </div>
-      <button>Add</button>
+      <button onClick={handleAddContact}>Add</button>
     </div>
   );
 }
