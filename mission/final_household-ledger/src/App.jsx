9f7bb9291b796973bb79723e5e9dca119bb8dcce
San Kim <aeginventor@gmail.com>
2026-01-05 22:57:39 +0900

    feat: 거래 상태관리 기능 구현

--- mission/final_household-ledger/src/App.jsx
+++ mission/final_household-ledger/src/App.jsx
@@ -4,6 +4,54 @@ import Home from "./pages/Home";
 import NewTransaction from "./pages/NewTransaction";
 import EditTransaction from "./pages/EditTransaction";
 import Notfound from "./pages/Notfound";
+import { createContext, useReducer, useRef, useEffect } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INIT":
+      return action.mockData;
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) == String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) != String(action.id));
+    default:
+      return state;
+  }
+}
+
+const mockData = [
+  {
+    id: 0,
+    name: "마라탕 & 꿔바로우",
+    amount: 59000,
+    type: "expense",
+    category: "🍚 식비",
+    date: new Date().getTime() + 1,
+  },
+  {
+    id: 1,
+    name: "월세",
+    amount: 500000,
+    type: "expense",
+    category: "🏠 생활",
+    date: new Date().getTime() + 2,
+  },
+  {
+    id: 2,
+    name: "월급",
+    amount: 3500000,
+    type: "income",
+    category: "🏢 급여",
+    date: new Date().getTime() + 3,
+  },
+];
+
+const TransactionStateContext = createContext();
+const TransactionDispatchContext = createContext();
 
 function App() {
   const nav = useNavigate();
@@ -12,26 +60,85 @@ function App() {
     nav("/new-transaction");
   };
 
+  const [transactions, setTransactions] = useReducer(reducer, []);
+  const idRef = useRef(3);
+
+  const onInitTransaction = (mockData) => {
+    setTransactions({
+      type: "INIT",
+      mockData,
+    });
+  };
+
+  const onCreateTransaction = (name, amount, type, category, date) => {
+    setTransactions({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        name,
+        amount,
+        type,
+        category,
+        date,
+      },
+    });
+  };
+
+  const onUpdateTransaction = (id, name, amount, type, category, date) => {
+    setTransactions({
+      type: "UPDATE",
+      data: {
+        id,
+        name,
+        amount,
+        type,
+        category,
+        date,
+      },
+    });
+  };
+
+  const onDeleteTransaction = (id) => {
+    setTransactions({
+      type: "DELETE",
+      id,
+    });
+  };
+
+  useEffect(() => {
+    onInitTransaction(mockData);
+  }, []);
+
   return (
     <div>
-      <div>
-        <Link to={"/"}>Home</Link>
-      </div>
-      <div>
-        <Link to={"/new-transaction"}>NewTransaction</Link>
-      </div>
-      <div>
-        <Link to={"/edit-transaction"}>EditTranscation</Link>
-      </div>
-      <div>
-        <button onClick={onClickButton}>NEW</button>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new-transaction" element={<NewTransaction />} />
-        <Route path="/edit-transaction/:id" element={<EditTransaction />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <TransactionStateContext.Provider value={transactions}>
+        <TransactionDispatchContext.Provider
+          value={{
+            onCreateTransaction,
+            onUpdateTransaction,
+            onDeleteTransaction,
+          }}
+        >
+          <div>
+            <Link to={"/"}>Home</Link>
+          </div>
+          <div>
+            <Link to={"/new-transaction"}>NewTransaction</Link>
+          </div>
+          <div>
+            <Link to={"/edit-transaction"}>EditTranscation</Link>
+          </div>
+          <div>
+            <button onClick={onClickButton}>NEW</button>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new-transaction" element={<NewTransaction />} />
+            <Route path="/edit-transaction/:id" element={<EditTransaction />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </TransactionDispatchContext.Provider>
+      </TransactionStateContext.Provider>
     </div>
   );
 }
