e380af1af5c79df3c8e41723e6f5ce6610a96ca8
San Kim <aeginventor@gmail.com>
2026-01-06 18:16:13 +0900

    feat(가계부): Home 페이지 UI 구현

--- mission/final_household-ledger/src/pages/Home.jsx
+++ mission/final_household-ledger/src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import TransactionList from "../components/TransactionList";
+import "./Home.css";
+import { useContext } from "react";
+import { TransactionStateContext } from "../App";
+import TransactionItem from "../components/TransactionItem";
+
+const getSortedTransactions = (transactions) => {
+  return transactions.toSorted((a, b) => Number(b.date) - Number(a.date));
+};
 
 const Home = () => {
+  const transactions = useContext(TransactionStateContext);
+
+  const sortedTransactions = getSortedTransactions(transactions);
+
   return (
-    <div>
-      <TransactionList />
+    <div className="Home">
+      <header>
+        <h1>한입 가계부</h1>
+        <div className="new_button">+ 작성하기</div>
+      </header>
+      <main className="transaction_list">
+        {sortedTransactions.map((item) => (
+          <TransactionItem key={item.id} {...item} />
+        ))}
+      </main>
     </div>
   );
 };
