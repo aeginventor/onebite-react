e7a7aceeef8f0f5e531b504363e4844d8fdf38c6
San Kim <aeginventor@gmail.com>
2026-01-08 15:05:05 +0900

    feat: 일기 수정 페이지 구현
    - 뒤로 가기 버튼
    - 삭제하기 버튼 클릭 시 해당 url parameter의 데이터 삭제 후 홈으로
      리다이렉션 (뒤로 가기 방지)
    - 홈에서 개별 일기 수정하기 버튼 클릭 시 해당 일기 데이터를 불러와서 렌더링
    - 일기 수정 페이지에서 작성완료 버튼 클릭 시 해당 일기 데이터 수정

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -2,9 +2,8 @@ import { useParams } from "react-router-dom";
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
 
-  return <div>{params.id}번 일기입니다.</div>;
+  return <div></div>;
 };
 
 export default Diary;
